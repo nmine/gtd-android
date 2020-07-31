025f46eef1893b2af3e3690ca95cbc31879188a1
nmine <nicolas.mine11@gmail.com>
2020-07-31 15:34:33 +0200

    add list view to the activity

--- app/src/main/java/be/nmine/gtd/MainActivity.kt
+++ app/src/main/java/be/nmine/gtd/MainActivity.kt
@@ -2,10 +2,47 @@ package be.nmine.gtd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import be.nmine.gtd.fragment.ActionsFragment
+import be.nmine.gtd.fragment.InboxFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener
+import kotlinx.android.synthetic.main.fragment_actions.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navigationBar = findViewById<BottomNavigationView>(R.id.navigation_bar)
+        navigationBar.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        val listView = recipe_list_view
+        val recipeList = arrayOf("test","test3","test4")
+        val listItems = arrayOfNulls<String>(recipeList.size)
+        for (i in 0 until recipeList.size) {
+            val recipe = recipeList[i]
+            listItems[i] = recipe
+        }
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
+        listView.adapter = adapter
+    }
+
+    private val mOnNavigationItemSelectedListener = OnNavigationItemSelectedListener {
+        when (it.itemId) {
+            R.id.menu_inbox -> {
+                val fragment = InboxFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment, fragment.javaClass.getSimpleName())
+                    .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.menu_actions -> {
+                val fragment = ActionsFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment, fragment.javaClass.getSimpleName())
+                    .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
     }
 }
---
+++ app/src/main/java/be/nmine/gtd/fragment/InboxFragment.kt
@@ -0,0 +1,21 @@
+package be.nmine.gtd.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import be.nmine.gtd.R
+
+class InboxFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? =
+        inflater.inflate(R.layout.fragment_inbox, container, false)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
