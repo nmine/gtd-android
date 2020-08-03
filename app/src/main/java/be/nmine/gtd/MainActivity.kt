85722c05dcadacad1c2803e780462da0e8671c39
nicolasmiine <nicolas.mine11@gmail.com>
2020-08-03 14:37:53 +0200

    add custom array adapter

--- app/src/main/java/be/nmine/gtd/MainActivity.kt
+++ app/src/main/java/be/nmine/gtd/MainActivity.kt
@@ -2,19 +2,33 @@ package be.nmine.gtd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import be.nmine.gtd.fragment.actions.ActionsFragment
 import be.nmine.gtd.fragment.inbox.InboxFragment
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.input.getInputField
+import com.afollestad.materialdialogs.input.input
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var inputField: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navigationBar = findViewById<BottomNavigationView>(R.id.navigation_bar)
         navigationBar.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-
+        fab_add_stuff.setOnClickListener {
+            val dialog: MaterialDialog = MaterialDialog(this).show {
+                input { dialog, text ->
+                    inputField = dialog.getInputField().text.toString()
+                }
+                positiveButton(R.string.app_name)
+            }
+            inputField = dialog.getInputField().text.toString()
+        }
     }
 
     private val mOnNavigationItemSelectedListener = OnNavigationItemSelectedListener {
