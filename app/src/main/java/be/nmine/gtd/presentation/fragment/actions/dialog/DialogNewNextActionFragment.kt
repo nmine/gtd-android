27e51bf118b81f0925073268ecc56bd6bf8596a7
nicolasmiine <nicolas.mine11@gmail.com>
2020-09-08 21:54:36 +0200

    create dialog fragment

--- app/src/main/java/be/nmine/gtd/presentation/fragment/actions/dialog/DialogNewNextActionFragment.kt
+++ app/src/main/java/be/nmine/gtd/presentation/fragment/actions/dialog/DialogNewNextActionFragment.kt
@@ -1,22 +1,27 @@
 package be.nmine.gtd.presentation.fragment.actions.dialog
 
-import android.app.Dialog
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import be.nmine.gtd.R
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.android.synthetic.main.dialog_next_action.*
+
 
 class DialogNewNextActionFragment : DialogFragment() {
 
-
-    override fun onCreateDialog(
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): Dialog {
+    ): View? =
+        inflater.inflate(R.layout.dialog_next_action, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val get:String = arguments?.get("title") as String
-        return MaterialAlertDialogBuilder(requireContext())
-            .setTitle(get)
-            .setView(R.layout.dialog_next_action)
-            .create()
+        dialog_text_action_item.text = get
+        dialog_button_create_next_action.setOnClickListener { print("clicked") }
     }
 
 }
