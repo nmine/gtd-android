aa07f00f82ac27a56d1467ca55f0075acfeb8f3a
nicolasmiine <nicolas.mine11@gmail.com>
2020-09-10 14:42:41 +0200

    Story : Delete NextActions implementing presentation , add toast message after delete

--- app/src/main/java/be/nmine/gtd/presentation/fragment/nextactions/listView/NextActionListAdapter.kt
+++ app/src/main/java/be/nmine/gtd/presentation/fragment/nextactions/listView/NextActionListAdapter.kt
@@ -3,9 +3,13 @@ package be.nmine.gtd.presentation.fragment.nextactions.listView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.PopupMenu
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatImageButton
 import androidx.fragment.app.FragmentActivity
 import be.nmine.gtd.R
+import be.nmine.gtd.domain.nextaction.NextAction
 import be.nmine.gtd.presentation.fragment.nextactions.viewModel.NextActionViewModel
 
 class NextActionListAdapter(
@@ -21,6 +25,28 @@ class NextActionListAdapter(
 
         val subtitleText = rowView.findViewById(R.id.description) as TextView
         subtitleText.text = description[position]
+        initThreeDotsMenu(rowView, subtitleText, position)
         return rowView
+    }
+
+    private fun initThreeDotsMenu(rowView: View?, subtitleText: TextView, position: Int) {
+        val three_dots = rowView?.findViewById(R.id.three_dots) as AppCompatImageButton
+        three_dots.setOnClickListener { view ->
+            val popup = PopupMenu(context, view)
+            popup.setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.menu_delete -> {
+                        val nextActionName = description[position]
+                        nextActionViewModel.deleteNextAction(NextAction(nextActionName))
+                        Toast.makeText(context,"Next Action : '$nextActionName' deleted", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    else -> false
+                }
+            }
+            popup.inflate(R.menu.next_action_menu_pop_up_action_list)
+            popup.show()
+        }
+
     }
 }
